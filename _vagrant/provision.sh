e3da7c72c3fea58367a0bd2f64041a174d30fb14
Thilo Maier <github@maier.asia>
2015-03-19 22:26:12 -0400

    Suppress console output during Vagrant provisioning (fixes #16)

--- _vagrant/provision.sh
+++ _vagrant/provision.sh
@@ -2,8 +2,12 @@
 
 # Vagrant provisioning script (executed as root)
 
-# install git
-apt-get -y update
-apt-get -y install curl git
+echo "Update package repositories..."
+apt-get -y update >/dev/null 2>&1
+echo "...done."
+
+echo "Install additional packages..."
+apt-get -y install curl git >/dev/null 2>&1
+echo "...done."
 
 su -c "source /vagrant/_vagrant/user-config.sh" vagrant
