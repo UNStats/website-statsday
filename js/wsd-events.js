c4dd66d560779538d5cb548be0877654d72d1600
sibs <sibylle.marxgut@onport80.com>
2015-11-19 13:56:52 +1000

    Filtering my 4 categories, design refinements

--- js/wsd-events.js
+++ js/wsd-events.js
@@ -5,73 +5,43 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-      
+
 /* Filtering by country, international organisation and other organisations ------ */
 $(function () {
-     /* Read all posts in the JSON file  */
+    /* Read all posts in the JSON file  */
     $.getJSON('js/events.json', function (data) {
-        /* Fill the posts template  */
         var template = $('#events-template').html();
-
-        /* Fill the countries template  */
-        var countries = data.locations;
-
-        /* If countries, get the country names associated with the codes  */
-        if (countries) {
-            countries.lookup = function (code) {
-                return _(this).where({ 'code': code }).pluck('name').first();
-            };
-        }
-
-        /* Fill the organisations template  */
-        var organisations = data.codes.organisation;
-
-        /* If organisations, get the organisation names or organisation type associated with the codes  */
-        if (organisations) {
-            /* Lookup by code */
-            organisations.lookup = function (code) {
-                return _(this).where({ 'code': code }).pluck('name').first();
-            };
-            /* Lookup by type (international/other) */
-            organisations.lookupByType = function (type) {
-                return _(this).where({ 'type': type }).pluck('code').value();
-            }
-        }
-
-        /* Take a post, add the provider/s and return the completed post  */
-        var addProvider = function (post) {
-            post.provider = function () {
-                var cs = _(this.countries || []).map(function (code) { return countries.lookup(code); });
-                var os = _(this.organisations || []).map(function (code) { return organisations.lookup(code); }).value();
-                return cs.concat(os).reduce(function (s, value) { return s += ', ' + value; });
-            };
-            return post;
-        }
-
-        /* Convert the markdown into HTML using the Lodash library */
-        var addHtml = function (post) {
-            post.html = function () {
-                return markdown.toHTML(this.text);
-            };
-            return post;
-        };
-
-        /* Call function getParameterByName to get the query strings and assign them to a variable [array] */
-        /* City  */
-        var c = getParameterByName('c');
+        var events = data.events;
         
-        /* Call function addProvider to add one or more providers to a post */
-        //var events = _(data.events).map(addHtml).map(addProvider);
-        var events = _(data.events).map(addHtml);
-
-        /* If countries */
-        if (c !== '') {
-            /* Get all posts by countries */
-            events = events.where({ 'city': [c] });
+        /* Call function getParameterByName to get the query strings and assign them to a variable [array] */
+        /* Location  */
+        var l = getParameterByName('l');
+        /* Tag */
+        var t = getParameterByName('t');
+        /* Event type */
+        var ty = getParameterByName('ty');
+               
+        /* If location */
+        if (l !== '') {
+            /* Get all events by location */
+            events = _.where(data.events, { 'city': l });
         }
-
-        /* Fill events template  */
-        var eventsHtml = Mustache.to_html(template, events.value());
-        $('#events').html(eventsHtml);
+        
+        /* If tag */
+        if (t !== '') {
+            /* Get all events by tag */
+            events = _.where(data.events, { 'tags': [t] });
+        }
+        
+        /* If location */
+        if (ty !== '') {
+            /* Get all events by location */
+            events = _.where(data.events, { 'type': ty });
+        }
+       
+        var items = Mustache.to_html(template, events);
+        $('#events').html(items);        
     });
 });
+        
+        
