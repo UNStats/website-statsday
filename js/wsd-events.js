71906fcc3cca70d4fbff908cb886c1ec334b92c5
sibs <sibylle.marxgut@onport80.com>
2015-11-25 14:35:08 +1000

    Detailed events pages, date formats and other improvments

--- js/wsd-events.js
+++ js/wsd-events.js
@@ -11,10 +11,19 @@ $(function () {
     /* Read all posts in the JSON file  */
     $.getJSON('js/events.json', function (data) {
         var template = $('#events-template').html();
+        var detailsTemplate = $('#details-template').html();
         var events = data.events;
-        var datesTemplate = $('#dates-template').html();
-        var items = Mustache.to_html(datesTemplate, events);
-        $('#dates').html(items); 
+        //var datesTemplate = $('#dates-template').html();
+        //var items = Mustache.to_html(datesTemplate, events);
+        //$('#dates').html(items); 
+        
+        /* Convert the markdown into HTML using the Lodash library */
+        var addHtml = function (event) {
+            event.html = function () {
+                return markdown.toHTML(this.description);
+            };
+            return event;
+        };
                 
         /* Call function getParameterByName to get the query strings and assign them to a variable [array] */
         /* Location  */
@@ -25,7 +34,12 @@ $(function () {
         var ty = getParameterByName('ty');
         /* date */
         var d = getParameterByName('d');
-               
+        /* ID */
+        var i = getParameterByName('i');
+        
+        /* Call function add HTML to markdown */
+        events = events.map(addHtml);
+        
         /* If location */
         if (l !== '') {
             /* Get all events by location */
@@ -50,8 +64,19 @@ $(function () {
             events = _.where(data.events, { 'start': d });
         }
 
-        items = Mustache.to_html(template, events);
+        var items = Mustache.to_html(template, events);
         $('#events').html(items);
+                
+        /* If ID, display only information for the relevant event */
+        if (i !== '') {
+            /* Get all events by date */
+            events = _.where(data.events, { 'id': i });
+            items = Mustache.to_html(detailsTemplate, events);
+            $('#event-details').html(items);
+            $('#events').addClass("hide");
+        }
+
+
 
 
         /* Format event dates */
@@ -63,6 +88,36 @@ $(function () {
                     jQuery(elem).val(jQuery.format.date(jQuery(elem).val(), shortDateFormat));
                 } else {
                     jQuery(elem).text(jQuery.format.date(jQuery(elem).text(), shortDateFormat));
+                }
+            });
+
+            var dayDateFormat = 'dd';
+
+            jQuery(".dayDateFormat").each(function (idx, elem) {
+                if (jQuery(elem).is(":input")) {
+                    jQuery(elem).val(jQuery.format.date(jQuery(elem).val(), dayDateFormat));
+                } else {
+                    jQuery(elem).text(jQuery.format.date(jQuery(elem).text(), dayDateFormat));
+                }
+            });
+
+            var monthDateFormat = 'MMMM';
+
+            jQuery(".monthDateFormat").each(function (idx, elem) {
+                if (jQuery(elem).is(":input")) {
+                    jQuery(elem).val(jQuery.format.date(jQuery(elem).val(), monthDateFormat));
+                } else {
+                    jQuery(elem).text(jQuery.format.date(jQuery(elem).text(), monthDateFormat));
+                }
+            });
+
+            var yearDateFormat = 'yyyy';
+
+            jQuery(".yearDateFormat").each(function (idx, elem) {
+                if (jQuery(elem).is(":input")) {
+                    jQuery(elem).val(jQuery.format.date(jQuery(elem).val(), yearDateFormat));
+                } else {
+                    jQuery(elem).text(jQuery.format.date(jQuery(elem).text(), yearDateFormat));
                 }
             });
         });
